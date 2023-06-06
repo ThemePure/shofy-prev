264bf68b5d4ac8db8d0c78dfabba94e4a908fb11
iqbal-web <iqbalgazi2021@gmail.com>
2023-06-06 16:04:40 +0600

    prev

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 "use client"
 import "../../styles/index.scss";
-export const metadata = {
-  title: 'Shofy - Multipurpose eCommerce Next js Template',
-  description: 'Shofy - Multipurpose eCommerce Next js Template',
-}
+// export const metadata = {
+//   title: 'Shofy - Multipurpose eCommerce Next js Template',
+//   description: 'Shofy - Multipurpose eCommerce Next js Template',
+// }
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+       <title>Shofy - Multipurpose eCommerce Next js Template</title>
         <link rel="icon" src="/public/favicon.ico" type="image/png" />
       </head>
       <body>{children}</body>
